4b6fd6f5665d4356554ee7e8d74be37272ca1881
coco <cocoxcomputerscience@gmail.com>
2024-03-11 04:51:20 -0400

    Allow the user to select buttons to play the game
    Before, the game ended when 5 rounds were completed.
    Now, the game ends when a player reaches 5 points.

--- script.js
+++ script.js
@@ -4,56 +4,87 @@ function getComputerChoice() {
     return(computerChoice);
 }
 
-// will play a single round of Rock Paper Scissors
-function playRound(playerSelection, computerSelection) {
+function playRound(e) {
+    let playerSelection = e.target.id;
+    let computerSelection = getComputerChoice();
+
     // strings for the result
     let result;
     let win = `You win! ${playerSelection} beats ${computerSelection}`;
     let lose = `You lose! ${playerSelection} loses to ${computerSelection}`;
     let tie = `Tie! You both chose ${playerSelection}`;
 
-    playerSelection = playerSelection.toLowerCase();
-
     if (playerSelection === computerSelection) {
-        return tie;
-    }
-    switch (playerSelection) {
-        case "rock":
-            result = computerSelection === "scissors" ? win : lose;
-            break;
-        case "paper":
-            result = computerSelection === "rock" ? win : lose;
-            break;
-        case "scissors":
-            result = computerSelection === "paper" ? win : lose;
-            break;
+        result = tie;
+    } else {
+        switch (playerSelection) {
+            case "rock":
+                result = computerSelection === "scissors" ? win : lose;
+                break;
+            case "paper":
+                result = computerSelection === "rock" ? win : lose;
+                break;
+            case "scissors":
+                result = computerSelection === "paper" ? win : lose;
+                break;
+        }
     }
 
-    (result === win) ? playerScore++ : computerScore++;
-    return result;
+    if (result === win) playerScore++;
+    if (result === lose) playerScore++;
+    resultText.textContent = `${playerScore}-${computerScore}\n${result}`;
+
+    if (playerScore === 5 || computerScore === 5) {
+        endGame();
+    } 
 }
 
-// will play 5 rounds of Rock Paper Scissors
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Input rock, paper, or scissors", "rock");
-        let computerChoice = getComputerChoice();
-        let roundResult = playRound(playerChoice, computerChoice);
-        console.log(roundResult);
-    }
+function endGame() {
+    let endResult = document.createElement("div");
+    resultText.appendChild(endResult);
+    endResult.textContent = playerScore > computerScore ? "Congratulations, you win the game!" : "Sorry, the computer won this game!";
 
-    // overall score
-    if (playerScore > computerScore) {
-        console.log(`${playerScore}-${computerScore} The player wins the game!`);
-    } else if (playerScore < computerScore) {
-        console.log(`${playerScore}-${computerScore} The player loses the game!`);
-    } else {
-        console.log(`${playerScore}-${computerScore} Tie!`);
-    }
+    // remove click events from the choices
+    choices.forEach(choice => choice.removeEventListener("click", playRound));
 }
 
 // global counter for overall score
 let playerScore = 0;
 let computerScore = 0;
 
-playGame();
+let resultText = document.querySelector("#result");
+let choices = document.querySelectorAll("#choices button");
+choices.forEach(choice => {
+    choice.addEventListener("click", playRound);
+});
+
+// UPDATE:
+// User now clicks on one of the buttons to initiate the game
+// Display the running score and announce a winner once 5 points has been met
+// playRound function will be the event handler for when a button is clicked
+
+/*
+HTML
+    1. add 3 buttons for the choices
+    2. add new game button
+
+
+JS
+event handler
+    1. add event listeners to all the buttons using event delegation
+    2. or use for...of or for each to 
+
+playRound(e)
+    1. playRound will now only have event object being passed as argument
+       let choice = event.target.id;
+    2. will call a new function if a player reaches 5 points
+
+endGame()
+    1. show the final result string (don't display round result maybe??)
+    2. remove listeners from the choice buttons
+
+LATER
+EXTRA newGame()
+    1. re-eneable event listeners for the choices buttons
+    2. clear the string result
+*/
